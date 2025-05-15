9946f7f78403bb044a72ec19a55395c6ee88ef5d
Uwais_Bayrakdar <144391684+Uwais-Bayrakdar@users.noreply.github.com>
2025-05-15 16:22:28 +0300

    Create Admin.h

---
+++ Admin.h
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef ADMIN_H
+#define ADMIN_H
+
+#define SECRET_CODE 5372
+
+void admin_panel(void);
+
+void insert(void);
+
+void delete(void);
+
+void update(void);
+
+void search(void);
+
+void print_list_admin(void);
+
+
+
+#endif // !ADMIN_H
